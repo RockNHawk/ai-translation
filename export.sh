d2dd50c7218237b046630b175004a78a63ea7fb7
mengru zhao <mrzhao@thoughtworks.com>
2018-07-16 13:38:11 +0800

    init for dev environment

---
+++ export.sh
@@ -0,0 +1,5 @@
+PYTHONIOENCODING=utf-8 python3 -m nmt.nmt \
+       --out_dir=/home/ubuntu/ai-translation/translate_model \
+       --infer_file=/home/ubuntu/ai-translation/data/test.en \
+       --export_path=/home/ubuntu/ai-translation/translate_model \
+       --ckpt_path=/home/ubuntu/ai-translation/translate_model
